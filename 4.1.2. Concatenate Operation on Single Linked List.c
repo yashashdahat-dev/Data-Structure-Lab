244d69843fe68f1f3455b828ba32fcbd21d43416
yashashdahat-dev <yashashdahat@gmail.com>
2026-02-10 09:26:00 +0530

    Add linked list operations and display function
    Implement functions for creating, adding, and concatenating single linked lists.

--- 4.1.2. Concatenate Operation on Single Linked List.c
+++ 4.1.2. Concatenate Operation on Single Linked List.c
@@ -1,1 +1,61 @@
+struct node {
+    int data;
+    struct node* next;
+};
 
+typedef struct node* NODE;
+
+NODE createNode(int x) {
+    //write your code here..
+NODE newNode = (NODE)malloc(sizeof(struct node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = x;
+    newNode->next = NULL;
+    return newNode;
+}
+
+NODE addNode(NODE first, int x) {
+    //write your code here..
+NODE newNode = createNode(x);
+    if (first == NULL) {
+        return newNode;
+	}
+    NODE temp = first;
+    while (temp->next != NULL) {
+        temp = temp->next;
+	}
+    temp->next = newNode;
+    return first;
+}
+
+NODE concatenate(NODE t1, NODE t2) {
+    //write your code here..
+if (t1 == NULL) return t2;
+    if (t2 == NULL) return t1;
+    
+    NODE temp = t1;
+    while (temp->next != NULL) {
+        temp = temp->next;
+	}
+    temp->next = t2;
+    
+    return t1;
+}
+
+void displayList(NODE first) {
+    //write your code here..
+if (first == NULL) {
+        printf("NULL\n");
+        return;
+    }
+    
+    NODE temp = first;
+    while (temp != NULL) {
+        printf("%d --> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
